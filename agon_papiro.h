ec09938a05cdaa297f93f09a1b66261db69a1c5e
th3exorcist <58277328+th3exorcist@users.noreply.github.com>
2020-05-28 17:29:27 -0300

    theme

---
+++ agon_papiro.h
@@ -0,0 +1,78 @@
+
+
+
+#include <stdio.h>   
+#include <unistd.h>   
+#include <stdarg.h>   
+#include <string.h>   
+
+#include "colorss.c"
+
+
+
+void drawAgon() {
+
+  char* color1 = BOLD_BLUE_COLOR;
+  char* color2 = BOLD_GREEN_COLOR;
+
+
+
+
+
+
+
+
+
+
+     printf("%s	█████╗  ██████╗  ██████╗ ███╗   ██╗\n", color1);
+     printf("%s	██╔══██╗██╔════╝ ██╔═══██╗████╗  ██║\n", color2);
+     printf("%s	███████║██║  ███╗██║   ██║██╔██╗ ██║\n", color1);
+     printf("%s	██╔══██║██║   ██║██║   ██║██║╚██╗██║\n", color2);
+     printf("%s	██║  ██║╚██████╔╝╚██████╔╝██║ ╚████║\n", color1);
+     printf("%s	╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝\n", color2);
+     printf("%s                                     \n", RESET_COLOR);
+
+
+
+printf(" \n" );
+printf(" \n" );
+printf(" \n" );
+
+printf(" \n" );
+printf("    _______________________-------------------                       `/. \n" );
+printf("  /:--__                                                              | \n" );
+printf(" ||< > |                                   ___________________________/ \n" );
+printf(" | /__/_________________-------------------                         | \n" );
+printf(" |                                                                  | \n" );
+printf("  |                    WELCOME FROM AGON                            | \n" );
+printf("  |                                                                  | \n" );
+printf("  |      'Ed Warren, Geralt e Kira acharam um Death Note,             | \n" );
+printf("   |        deixado pelo Shinigami Agon. Para decidir quem pode         | \n" );
+printf("   |      usar o Death Note para escrever um nome                      | \n" );
+printf("   |        eles disputam.                                              | \n" );
+printf("   |      						                 | \n" );
+printf("    |       AGON, demônio dos concursos e das disputas                   | \n" );
+printf("    |       será o mediador.                                             | \n" );
+printf("    |                                                                    | \n" );
+printf("   |                                              ____________________|_ \n" );
+printf("   |  ___________________-------------------------                      `. \n" );
+printf("   |/`--_                                                                 | \n" );
+printf("   ||[ ]||                                            ___________________/ \n" );
+printf("    .===/___________________-------------------------- \n" );
+printf(" \n" );
+
+
+
+
+
+
+}
+/*
+int main(){
+
+draw();
+
+return 0;
+
+}
+*/
